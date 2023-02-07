860fdceadf085ad49e42408a76c9ea3c477c93a6
Georg <geggihoe@gmail.com>
2023-02-07 16:16:46 +0100

    refactoring

--- frontend/ngmug/libs/ngmug/feature-about-ngmug/src/lib/components/get-started/get-started.component.ts
+++ frontend/ngmug/libs/ngmug/feature-about-ngmug/src/lib/components/get-started/get-started.component.ts
@@ -1,35 +1,12 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { computePosition, flip } from '@floating-ui/dom';
+import { Component, OnInit } from '@angular/core';
 import { ROUTING } from '@ngmug/shared/ngmug-utils';
 
 @Component({
   selector: 'ngmug-get-started',
   templateUrl: './get-started.component.html',
 })
-export class GetStartedComponent implements OnInit, AfterViewInit {
-  @ViewChild('btn') btn!: ElementRef;
-  @ViewChild('tooltip') tooltip!: ElementRef;
-
+export class GetStartedComponent implements OnInit {
   rxjsAboutRoute = ROUTING.getNavRoute((p) => p.rxjs.getstarted);
 
   ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    computePosition(this.btn.nativeElement, this.tooltip.nativeElement, {
-      placement: 'top',
-      middleware: [flip()],
-    }).then(({ x, y }) => {
-      console.log('log');
-      Object.assign(this.tooltip.nativeElement.style, {
-        left: `${x}px`,
-        top: `${y}px`,
-      });
-    });
-  }
 }
